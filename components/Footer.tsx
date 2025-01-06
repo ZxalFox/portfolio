e580caa971615d22244f34122c5b64ea9318b62a
Ayron Sanfra <ayron.marinho@estudante.ufla.br>
2025-01-06 20:38:34 -0300

    feat: added new style

---
+++ components/Footer.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+
+const Footer = () => {
+  return (
+    <footer className="w-full mb-[100px] md:mb-5 pb-10" id="contact">
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to take
+          <span className="text-purple"> your </span> digital presence to the
+          next level?
+        </h1>
+
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me today and let&apos;s discuss how we can work together
+          to make your business stand out online.
+        </p>
+        <a href="mailto:ayronsanfra9@gmail.com">
+          <MagicButton
+            title="Get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col gap-y-5 items-center justify-between">
+        <p className="md:text-base text-sm font-light md:font-normal">
+          Copyright © Ayron Sanfra 2024
+        </p>
+
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile, index) => (
+            <div
+              key={index}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={profile.img} alt={profile.img} width={20} height={20} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,154 @@
+/* eslint-disable */
 import type { Config } from "tailwindcss";
+import svgToDataUri from "mini-svg-data-uri";
+import colors from "tailwindcss/colors";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
-export default {
+const config: Config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./data/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+          200: "rgba(17, 25, 40, 0.75)",
+          300: "rgba(255, 255, 255, 0.125)",
+        },
+        white: {
+          DEFAULT: "#FFF",
+          100: "#BEC1DD",
+          200: "#C1C2D3",
+        },
+        blue: {
+          "100": "#E4ECFF",
+        },
+        purple: "#CBACF9",
+        ...generateColorVariants([
+          "border",
+          "input",
+          "ring",
+          "background",
+          "foreground",
+          "primary",
+          "secondary",
+          "destructive",
+          "muted",
+          "accent",
+          "popover",
+          "card",
+        ]),
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: generateKeyframes(),
+      animation: generateAnimations(),
+    },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+    gridAndDotBackgrounds,
+  ],
+};
+
+// Helper Functions
+function generateColorVariants(keys: string[]) {
+  const variants: Record<string, unknown> = {};
+  keys.forEach((key) => {
+    variants[key] = {
+      DEFAULT: `hsl(var(--${key}))`,
+      foreground: `hsl(var(--${key}-foreground))`,
+    };
+  });
+  return variants;
+}
+
+function generateKeyframes() {
+  return {
+    "accordion-down": {
+      from: { height: "0" },
+      to: { height: "var(--radix-accordion-content-height)" },
+    },
+    "accordion-up": {
+      from: { height: "var(--radix-accordion-content-height)" },
+      to: { height: "0" },
+    },
+    shimmer: {
+      from: { backgroundPosition: "0 0" },
+      to: { backgroundPosition: "-200% 0" },
+    },
+    spotlight: {
+      "0%": { opacity: "0", transform: "translate(-72%, -62%) scale(0.5)" },
+      "100%": { opacity: "1", transform: "translate(-50%,-40%) scale(1)" },
+    },
+    moveVertical: {
+      "0%": { transform: "translateY(-50%)" },
+      "50%": { transform: "translateY(50%)" },
+      "100%": { transform: "translateY(-50%)" },
+    },
+    scroll: { to: { transform: "translate(calc(-50% - 0.5rem))" } },
+  };
+}
+
+function generateAnimations() {
+  return {
+    "accordion-down": "accordion-down 0.2s ease-out",
+    "accordion-up": "accordion-up 0.2s ease-out",
+    spotlight: "spotlight 2s ease .75s 1 forwards",
+    shimmer: "shimmer 2s linear infinite",
+    moveVertical: "moveVertical 30s ease infinite",
+    scroll: "scroll var(--animation-duration, 40s) linear infinite",
+  };
+}
+
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({ ":root": newVars });
+}
+
+function gridAndDotBackgrounds({ matchUtilities, theme }: any) {
+  matchUtilities(
+    {
+      "bg-grid": (value: string) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="100" height="100" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+        )}")`,
+      }),
+      "bg-grid-small": (value: string) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+        )}")`,
+      }),
+      "bg-dot": (value: string) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+        )}")`,
+      }),
+    },
+    { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+  );
+}
+
+export default config;
